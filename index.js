4884dd58c825b855579cf0281af41e895c366406
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-04-27 23:19:48 +0100

    raydium setup

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { Web3 } from "web3";
+import dotenv from "dotenv"
+import axios from "axios"
+dotenv.config()
 
 const web3 = new Web3("https://eth-mainnet.g.alchemy.com/v2/4tKqe9wUH_vnTMclsNMVmqgqb86VXbgz");
+const ETHERSCAN_KEY = process.env.ETHERSCAN_API_KEY
+
 
 async function main() {
   const block = await web3.eth.getBlockNumber();
@@ -14,8 +19,13 @@ async function main() {
     return code !== "0x" && code !== "0x0";
   }
 
-  const result = await isContract("0xdAC17F958D2ee523a2206206994597C13D831ec7");
+  const result = await isContract("0xE592427A0AEce92De3Edee1F18E0157C05861564");
   console.log(result ? "Contract address" : "EOA address");
 }
 
-main();
+axios.get(`https://api.etherscan.io/v2/api?chainid=1&module=contract&action=getabi&address=${"0xE592427A0AEce92De3Edee1F18E0157C05861564"}&apikey=${ETHERSCAN_KEY}`,{
+    address : "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+}).then (response => {
+   // console.log(response.data)
+})
+//main();
---
+++ raydium.js
@@ -0,0 +1,5 @@
+import { Transaction, VersionedTransaction, sendAndConfirmTransaction } from '@solana/web3.js'
+import { NATIVE_MINT } from '@solana/spl-token'
+import axios from 'axios'
+import { connection, owner, fetchTokenAccountData } from '../config'
+import { API_URLS } from '@raydium-io/raydium-sdk-v2'
