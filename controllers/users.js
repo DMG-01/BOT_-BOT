5217ca9967e6488947b4aec18ad4795ca2c2f761
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-05-03 22:53:29 +0100

    crypto hasinh algorithm

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
 const { Keypair } = require('@solana/web3.js');
 const ethWeb3 = require('web3');
 const statusCodes = require('http-status-codes');
+const crypto = require("crypto")
+require("dotenv").config()
 
 const signUp = async (req, res) => {
-    console.log("................")
+    
   try {
     const { userName, emailAddress } = req.body;
 
@@ -43,6 +45,30 @@ const signUp = async (req, res) => {
       msg: 'INTERNAL_ERROR_OCCURRED',
     });
   }
+};
+
+
+const algorithm = "aes-256-ctr";
+const secretKey = Buffer.from(process.env.CRYPTO_SECRET_KEY, "hex"); // Must be 32 bytes
+const iv = Buffer.from(process.env.CRYPTO_SECRET_IV, "hex"); // Must be 16 bytes
+
+
+const encrypt = (text) => {
+    const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+    return `${iv.toString("hex")}:${encrypted.toString("hex")}`;
+};
+
+
+const decrypt = (hash) => {
+    const [ivHex, encryptedData] = hash.split(":");
+    const ivBuffer = Buffer.from(ivHex, "hex");
+
+    if (ivBuffer.length !== 16) throw new Error("Invalid IV length"); // Ensuring IV length is correct
+
+    const decipher = crypto.createDecipheriv(algorithm, secretKey, ivBuffer);
+    const decrypted = Buffer.concat([decipher.update(Buffer.from(encryptedData, "hex")), decipher.final()]);
+    return decrypted.toString();
 };
 
 module.exports = { signUp };
--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 const sequelize = require("../connectDb")
 const {DataTypes} = require("sequelize")
-
+const bcrypt = require("bcrypt")
 const user = sequelize.define("users", {
     userName : {
         type : DataTypes.STRING, 
@@ -27,6 +27,25 @@ const user = sequelize.define("users", {
         type : DataTypes.TEXT
     }
 })
+user.beforeCreate(async (userInstance) => {
+    const salt = await bcrypt.genSalt(10);
+    userInstance.password = await bcrypt.hash(userInstance.password, salt);
+  });
+  
+  user.prototype.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+  };
+  
+  user.prototype.createJWT = function () {
+    return jwt.sign(
+      { userId: this.userId, role: this.role },
+      process.env.GUARD_JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+  };
+
+  
+
 
 
 module.exports = user
